d5710860396113deef32c2f684637af14454cdea
Alyce Sousa <alyce.cristine.s@gmail.com>
2021-04-11 04:22:59 -0300

    feat: add server and some libs

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,6 +2,7 @@ import { create } from 'apisauce';
 
 const api = create({
   // baseURL: 'http://localhost:5001/',
+  baseURL: 'http://206.189.205.49/api',
 });
 
 // api.addRequestTransform(request => {
